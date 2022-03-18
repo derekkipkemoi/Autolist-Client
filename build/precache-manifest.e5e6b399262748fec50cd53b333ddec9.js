ad3cf16ebf7ecbf6df30ff349da4c9165d8ac592
derekkipkemoi <derekkipkemoi@gmail.com>
2022-03-18 19:14:57 +0300

    React production build

---
+++ build/precache-manifest.e5e6b399262748fec50cd53b333ddec9.js
@@ -0,0 +1,218 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a12a3dfe20e71b47059534b0c29498a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9a4008de9e13a6e45792",
+    "url": "/static/css/2.40a775a0.chunk.css"
+  },
+  {
+    "revision": "8f4102d7a4999981280c",
+    "url": "/static/css/main.069d7c7f.chunk.css"
+  },
+  {
+    "revision": "9a4008de9e13a6e45792",
+    "url": "/static/js/2.d64991cd.chunk.js"
+  },
+  {
+    "revision": "1101f097300a0477c74bafcb9553b7fc",
+    "url": "/static/js/2.d64991cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f4102d7a4999981280c",
+    "url": "/static/js/main.f88c536c.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "9305395a50f777823c8d0296f671c601",
+    "url": "/static/media/4.9305395a.jpg"
+  },
+  {
+    "revision": "0fba52241a60e4ccecbbf57b3e777955",
+    "url": "/static/media/Lipa-na-Mpesa.0fba5224.png"
+  },
+  {
+    "revision": "6207e6f223cf76469db4574c1f2735b8",
+    "url": "/static/media/audi.6207e6f2.svg"
+  },
+  {
+    "revision": "21dc08086be1680302a5508c88e8964c",
+    "url": "/static/media/bmw.21dc0808.svg"
+  },
+  {
+    "revision": "5e006e8614f3d4acab061ea31fd69dc0",
+    "url": "/static/media/brand.5e006e86.svg"
+  },
+  {
+    "revision": "f03fb53795fe846fd4f2441a55fde79e",
+    "url": "/static/media/calendar.f03fb537.svg"
+  },
+  {
+    "revision": "77f1dd26ecdf77d70c15ff9bb245a40e",
+    "url": "/static/media/car-dashboard.77f1dd26.svg"
+  },
+  {
+    "revision": "32f21248d67f64531240bd7e80953dfa",
+    "url": "/static/media/car-front.32f21248.svg"
+  },
+  {
+    "revision": "e244c83dd1fcfc744a695acc431056a1",
+    "url": "/static/media/car-interior.e244c83d.svg"
+  },
+  {
+    "revision": "a358952748f6bf50604f2316eea1c04d",
+    "url": "/static/media/car-left.a3589527.svg"
+  },
+  {
+    "revision": "510f5548a47540980e63ca82a9df5e98",
+    "url": "/static/media/car-rear.510f5548.svg"
+  },
+  {
+    "revision": "f6f52ee1fbbfcf10983159554057dd18",
+    "url": "/static/media/car-right.f6f52ee1.svg"
+  },
+  {
+    "revision": "3d95e9ca62d3b454d7fc5040ac9bbfbc",
+    "url": "/static/media/car.3d95e9ca.svg"
+  },
+  {
+    "revision": "7f20ed64c1523347071f1ffd2cbea75d",
+    "url": "/static/media/car_hatchback.7f20ed64.svg"
+  },
+  {
+    "revision": "57323271f53e734c9cedfd76359ff79d",
+    "url": "/static/media/carlogo2.57323271.png"
+  },
+  {
+    "revision": "1069dc5ad92277180d3a38b01475c97d",
+    "url": "/static/media/color.1069dc5a.svg"
+  },
+  {
+    "revision": "59339dd6b04f9ec5c2daf6e650ff9e37",
+    "url": "/static/media/engine.59339dd6.svg"
+  },
+  {
+    "revision": "bfee56135980cadd4ad1ccd2926f5830",
+    "url": "/static/media/facebook.bfee5613.svg"
+  },
+  {
+    "revision": "5a5ec88ca8cebbd59bc908f2b5fc0722",
+    "url": "/static/media/fuel.5a5ec88c.svg"
+  },
+  {
+    "revision": "3bd9039e626fce72efd49656fd3fca87",
+    "url": "/static/media/furnitures.3bd9039e.svg"
+  },
+  {
+    "revision": "5bcad3ae8f1b25b5e20ada1fcafc1051",
+    "url": "/static/media/gauge.5bcad3ae.svg"
+  },
+  {
+    "revision": "627c7649c67aadefb60081ff9dfd0bcd",
+    "url": "/static/media/gear_box.627c7649.svg"
+  },
+  {
+    "revision": "7a7dd387a6163243744114bdcdb4c27f",
+    "url": "/static/media/google-play.7a7dd387.svg"
+  },
+  {
+    "revision": "6044f129c293b4e73394fd2877856e33",
+    "url": "/static/media/google.6044f129.svg"
+  },
+  {
+    "revision": "c56db5ed9331901ae8a91d1bc8627bfb",
+    "url": "/static/media/honda.c56db5ed.svg"
+  },
+  {
+    "revision": "ec3a604cc495a5e575c145fceb7ff56a",
+    "url": "/static/media/instagram.ec3a604c.svg"
+  },
+  {
+    "revision": "9fdae49f89df33efcf2ae495bcee7d86",
+    "url": "/static/media/isuzu.9fdae49f.svg"
+  },
+  {
+    "revision": "6398bac916494f81e79c9529800895cf",
+    "url": "/static/media/land-rover.6398bac9.svg"
+  },
+  {
+    "revision": "c1dc96c3f43b9262420e18b8ec8b6e17",
+    "url": "/static/media/like.c1dc96c3.svg"
+  },
+  {
+    "revision": "9aea5e2af3aee227a20096dde46fa499",
+    "url": "/static/media/mazda.9aea5e2a.svg"
+  },
+  {
+    "revision": "843b4fbbd8a05b66e0e3234dbc5c9d36",
+    "url": "/static/media/mercedes-benz.843b4fbb.svg"
+  },
+  {
+    "revision": "859f0931edac43b2e6bd5933685f6efa",
+    "url": "/static/media/microbus.859f0931.svg"
+  },
+  {
+    "revision": "da8b63dfff82dc314e0e091435b986b1",
+    "url": "/static/media/mitsubishi.da8b63df.svg"
+  },
+  {
+    "revision": "dc04874a54371d51dc351340d18c12ff",
+    "url": "/static/media/motorcycle.dc04874a.svg"
+  },
+  {
+    "revision": "ea98165f01e8620e343d521d482e0550",
+    "url": "/static/media/new-product.ea98165f.svg"
+  },
+  {
+    "revision": "7f4d4a773a4ebc7fea480f2c25a6ae85",
+    "url": "/static/media/nissan.7f4d4a77.svg"
+  },
+  {
+    "revision": "cd1d5b14ee016b979da54c0e201605f3",
+    "url": "/static/media/premium.cd1d5b14.svg"
+  },
+  {
+    "revision": "208f78c32157bfd74467ef312c2367ef",
+    "url": "/static/media/scissors.208f78c3.svg"
+  },
+  {
+    "revision": "19064d570b5f1b1c2908df463d81c759",
+    "url": "/static/media/shapes.19064d57.svg"
+  },
+  {
+    "revision": "755224d7530ddae078c678a24d2f5cbb",
+    "url": "/static/media/subaru.755224d7.svg"
+  },
+  {
+    "revision": "f9d38b1646224605ab9878024a23948b",
+    "url": "/static/media/suv.f9d38b16.svg"
+  },
+  {
+    "revision": "38dc384aeadc19f8a8343dbfa8b524c7",
+    "url": "/static/media/tax.38dc384a.svg"
+  },
+  {
+    "revision": "56054ece977fd3c56c1d91b1f335abb0",
+    "url": "/static/media/toyota.56054ece.svg"
+  },
+  {
+    "revision": "331edc3c036e9fcfbc3d3df27ad5927a",
+    "url": "/static/media/truck.331edc3c.svg"
+  },
+  {
+    "revision": "91fae390750a6c3a3c883309f4c4e74f",
+    "url": "/static/media/twitter.91fae390.svg"
+  },
+  {
+    "revision": "7982e005281f4b82aa83e63e7af92663",
+    "url": "/static/media/volkswagen.7982e005.svg"
+  },
+  {
+    "revision": "ddde4a1aadefa840abbe045333b6a241",
+    "url": "/static/media/wagon.ddde4a1a.svg"
+  }
+]);
